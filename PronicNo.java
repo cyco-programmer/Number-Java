19e639e09dabff9edbb51fc6a0044ac9551626c4
Aritra Ghosh <104589382+cyco-programmer@users.noreply.github.com>
2023-04-30 20:28:41 +0530

    pronic no

---
+++ PronicNo.java
@@ -0,0 +1,25 @@
+// pronic no is the no which is the product of two consecutive integers
+import java.util.*;
+public class Pronic_No
+{
+    int n,i=1,p=0;
+    void display()
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a no");
+        n=sc.nextInt();
+        int flag=0;
+        for(i=0;i<n;i++)
+        {
+            if(i*(i+1)==n)
+            {
+                flag=1;
+                break;
+            }
+        }
+        if(flag==1)
+        System.out.println("Pronic no");
+        else
+        System.out.println("not a pronic no");
+    }
+}
