6c92d97b2938c62b3fcfe05b8a5c361c13a32c36
Aritra Ghosh <104589382+cyco-programmer@users.noreply.github.com>
2023-07-04 19:35:50 +0530

    Update FibonacciNumber.java

--- FibonacciNumber.java
+++ FibonacciNumber.java
@@ -1,39 +1,22 @@
 import java.util.Scanner;
 
-/**
- * This class prints n terms of the fibonacci series.
- * In fibonacci series, next number is the sum of previous
- * two numbers.
- * @author Sayan Biswas
- * @version 06.04.2022
- */
+
 public class FibonacciSeries {
 
-    /**
-     * Stores number of terms
-     */
     private int x;
 
-    /**
-     * Initializes instance variables
-     */
+
     private FibonacciSeries() {
         x=0;
     }
 
-    /**
-     * Inputs number of terms from user
-     */
     private void input() {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of terms: ");
         x=Integer.parseInt(sc.next());
     }
 
-    /**
-     * Calculates and prints out the Fibonacci series
-     * @param n Number of terms
-     */
+
     private void compute(int n) {
         int a1 = 0, a2 = 1, a3;
         //Printing first two terms
@@ -46,10 +29,6 @@ public class FibonacciSeries {
         }
     }
 
-    /**
-     * Calls other methods
-     * @param args Arguments passed to main method
-     */
     public static void main(String[] args) {
         FibonacciSeries ob = new FibonacciSeries();
         ob.input();
