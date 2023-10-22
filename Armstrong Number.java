81a9773d652698aa19d16efdd2f208d92c8d176d
cyco_programmer <ghoshranu465@gmail.com>
2023-10-22 09:04:16 +0530

    update error

--- Armstrong Number.java
+++ Armstrong Number.java
@@ -1,45 +1,34 @@
 import java.util.Scanner;
 
-
 public class ArmstrongNumber {
-    
-    private int number;
-
-    private ArmstrongNumber() {
-        number = 0;
-    }
-
-    private void input() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number: ");
-        number = Integer.parseInt(sc.nextLine());
-    }
-
-    private int numberOfDigits(int n) {
-        return (int)Math.floor(Math.log10(n))+1;
-    }
-
-    private boolean armstrongCheck(int a) {
-        int sum = 0;
-        while(a > 0) {
-            sum += (int)Math.pow(a % 10,
-                    numberOfDigits(number));
-            a /= 10;
-        }
-        return sum == number;
-    }
-
-    private void display() {
-
-        System.out.println(armstrongCheck(number) ?
-                "Number is armstrong." :
-                "Number is not armstrong.");
-    }
-
-   
     public static void main(String[] args) {
-        ArmstrongNumber ob = new ArmstrongNumber();
-        ob.input();
-        ob.display();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        scanner.close();
+        
+        if (isArmstrong(number)) {
+            System.out.println(number + " is an Armstrong number.");
+        } else {
+            System.out.println(number + " is not an Armstrong number.");
+        }
+    }
+
+    public static boolean isArmstrong(int num) {
+        int originalNumber, remainder, result = 0, n = 0;
+        originalNumber = num;
+
+        // Find the number of digits in the given number
+        for (originalNumber = num; originalNumber != 0; originalNumber /= 10, ++n);
+
+        originalNumber = num;
+        while (originalNumber != 0) {
+            remainder = originalNumber % 10;
+            result += Math.pow(remainder, n);
+            originalNumber /= 10;
+        }
+
+        // If the result is the same as the input number, it's an Armstrong number
+        return result == num;
     }
 }
