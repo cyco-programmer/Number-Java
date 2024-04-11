bc17aaa7f7656842cda18cdd7d5ae573dfa62a15
cyco_programmer <ghoshranu465@gmail.com>
2024-04-11 20:12:57 +0530

    update files

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -1,29 +1,30 @@
 import java.util.Scanner;
-
-public class PalindromeNumberChecker {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
-        scanner.close();
-
-        if (isPalindrome(number)) {
-            System.out.println(number + " is a palindrome number.");
-        } else {
-            System.out.println(number + " is not a palindrome number.");
+public class Palindrome_Array
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        System.out.println("Enter size of array");
+        n=sc.nextInt();
+        int ar[]=new int[n],d,r,i,copy;
+        System.out.println("Enter" +n+ "integers");
+        for(i=0;i<n;i++)
+        {
+            ar[i]=sc.nextInt();
         }
-    }
-
-    public static boolean isPalindrome(int num) {
-        int originalNumber = num;
-        int reversedNumber = 0;
-
-        while (num != 0) {
-            int remainder = num % 10;
-            reversedNumber = reversedNumber * 10 + remainder;
-            num = num / 10;
+        for(i=0;i<n;i++)
+        {
+            copy=ar[i];
+            r=0;
+            while(copy!=0)
+            {
+                d=copy%10;
+                r=r*10+d;
+                copy/=10;
+            }
+            if(r==ar[i])
+            System.out.println(ar[i]+" is palindrome");
         }
-
-        return originalNumber == reversedNumber;
     }
 }
